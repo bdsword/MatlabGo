2b63c100a0011097a2060a5e002fc20fcffb23bf
qaws01395 <qaws01395@github.com>
2014-06-09 02:10:56 +0800

    add checkhistory.m & checkhistory.fig

---
+++ checkhistory.m
@@ -0,0 +1,77 @@
+function varargout = checkhistory(varargin)
+    gui_Singleton = 1;
+    gui_State = struct('gui_Name',       mfilename, ...
+                       'gui_Singleton',  gui_Singleton, ...
+                       'gui_OpeningFcn', @checkhistory_OpeningFcn, ...
+                       'gui_OutputFcn',  @checkhistory_OutputFcn, ...
+                       'gui_LayoutFcn',  [] , ...
+                       'gui_Callback',   []);
+    if nargin && ischar(varargin{1})
+        gui_State.gui_Callback = str2func(varargin{1});
+    end
+
+    if nargout
+        [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+    else
+        gui_mainfcn(gui_State, varargin{:});
+    end
+
+function checkhistory_OpeningFcn(hObject, eventdata, handles, varargin)
+handles.output = hObject;
+
+init
+
+set(gcf,'CurrentAxes',handles.axes1);
+imageData = imread('ChessBoard.jpg');
+imageHandle = image(imageData);
+hold on;
+set(gca,'xtick',[],'ytick',[]);
+axis square;
+set(gcf,'CurrentAxes',handles.chessPaintBoard);
+set(handles.chessPaintBoard,'xtick',[],'ytick',[]);
+set(handles.chessPaintBoard,'color','none');
+axis square;
+
+% bg image
+pic_bg = imread('gamebg.png');
+image(pic_bg,'parent',handles.axes_gmbg)
+set(handles.axes_gmbg,'xtick',[],'ytick',[]);
+
+%--- four image button and set button----
+picBack = imread('back.png');
+backImhandles = image(picBack,'parent',handles.axesBack);
+set(handles.axesBack,'xtick',[],'ytick',[]);
+set(backImhandles, 'ButtonDownFcn', @back_ClickFcn);
+
+picNext = imread('next.png');
+nextImhandles = image(picNext,'parent',handles.axesNext);
+set(handles.axesNext,'xtick',[],'ytick',[]);
+set(nextImhandles, 'ButtonDownFcn', @next_ClickFcn);
+%------------------
+
+handles.chessBoard = imageHandle;
+guidata(hObject, handles);
+
+
+function varargout = checkhistory_OutputFcn(hObject, eventdata, handles) 
+varargout{1} = handles.output;
+
+
+function back_ClickFcn(hObject, eventdata)
+
+handles = guidata(hObject);
+
+
+function next_ClickFcn(hObject, eventdata)
+
+handles = guidata(hObject);
+
+
+%======================================
+
+
+% --- Executes on mouse press over axes background.
+function chessPaintBoard_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to chessPaintBoard (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
